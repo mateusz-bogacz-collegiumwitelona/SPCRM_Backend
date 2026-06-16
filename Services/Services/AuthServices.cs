e9b1f737d3644e2982fea7c731e572de5490c109
mateusz-bogacz-collegiumwitelona <mateusz.bogacz-drewniak@studenci.collegiumwitelona.pl>
2026-06-16 20:32:41 +0200

    add logger to AuthServices

--- Services/Services/AuthServices.cs
+++ Services/Services/AuthServices.cs
@@ -6,6 +6,7 @@ using DTO.Response;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Services.Interfaces;
 
 namespace Services.Services
@@ -14,88 +15,81 @@ namespace Services.Services
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly TokenServices _token;
+        private readonly ILogger<AuthServices> _logger;
 
         public AuthServices(
             UserManager<ApplicationUser> userManager,
-            TokenServices token
+            TokenServices token,
+            ILogger<AuthServices> logger
             )
         {
             _userManager = userManager;
             _token = token;
+            _logger = logger;
         }
 
         public async Task<Result<AuthResponse>> LoginAsync(LoginRequest request)
         {
-            try
+            var user = await _userManager.Users.FirstOrDefaultAsync(u =>
+                u.Email == request.Name ||
+                u.NormalizedUserName == request.Name.Trim().ToUpper()
+            );
+
+
+            bool isPasswordValidate = false;
+
+
+            if (user != null)
             {
+                isPasswordValidate = await _userManager.CheckPasswordAsync(user, request.Password);
+            }
 
-
-                var user = await _userManager.Users.FirstOrDefaultAsync(u =>
-                    u.Email == request.Name ||
-                    u.NormalizedUserName == request.Name.Trim().ToUpper()
-                );
-
-
-                bool isPasswordValidate = false;
-
-
-                if (user != null)
-                {
-                    isPasswordValidate = await _userManager.CheckPasswordAsync(user, request.Password);
-                }
-
-                if (user == null || !isPasswordValidate)
-                {
-                    return Result<AuthResponse>.Failure(
-                        "Invalid username or password.",
-                        ErrorCodes.InvalidCredentials,
-                        StatusCodes.Status401Unauthorized
-                        );
-                }
-
-                if (!user.EmailConfirmed)
-                {
-                    return Result<AuthResponse>.Failure(
-                        "Email is not confirmed.",
-                        ErrorCodes.EmailNotConfirmed,
-                        StatusCodes.Status403Forbidden
-                        );
-                }
-
-                var roles = await _userManager.GetRolesAsync(user);
-
-                if (roles == null || !roles.Any())
-                {
-                    return Result<AuthResponse>.Failure(
-                        "User has no roles assigned.",
-                        ErrorCodes.NoRolesAssigned,
-                        StatusCodes.Status403Forbidden
-                        );
-                }
-
-                string token = _token.CreateJwtToken(user, roles);
-
-                return Result<AuthResponse>.Success(
-                    "Login successful.",
-                    StatusCodes.Status200OK,
-                    new AuthResponse
-                    {
-                        Token = token,
-                        UserId = user.Id,
-                        Email = user.Email ?? "",
-                        UserName = user.UserName ?? "",
-                        Roles = roles
-                    }
+            if (user == null || !isPasswordValidate)
+            {
+                _logger.LogInformation("Invalid username or password.");
+                return Result<AuthResponse>.Failure(
+                    "Invalid username or password.",
+                    ErrorCodes.InvalidCredentials,
+                    StatusCodes.Status401Unauthorized
                     );
             }
-            catch (Exception)
+
+            if (!user.EmailConfirmed)
             {
+                _logger.LogInformation("User: {userName} has not confirmed email.", user.UserName);
                 return Result<AuthResponse>.Failure(
-                    "An error occurred during login.",
-                    ErrorCodes.InternalError,
-                    StatusCodes.Status500InternalServerError
-                );
+                    "Email is not confirmed.",
+                    ErrorCodes.EmailNotConfirmed,
+                    StatusCodes.Status403Forbidden
+                    );
             }
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            if (roles == null || !roles.Any())
+            {
+                _logger.LogError("User: {userName}  has no roles assigned.", user.UserName);
+                return Result<AuthResponse>.Failure(
+                    "User has no roles assigned.",
+                    ErrorCodes.NoRolesAssigned,
+                    StatusCodes.Status403Forbidden
+                    );
+            }
+
+            string token = _token.CreateJwtToken(user, roles);
+
+            return Result<AuthResponse>.Success(
+                "Login successful.",
+                StatusCodes.Status200OK,
+                new AuthResponse
+                {
+                    Token = token,
+                    UserId = user.Id,
+                    Email = user.Email ?? "",
+                    UserName = user.UserName ?? "",
+                    Roles = roles
+                }
+            );
         }
     }
 }
